a4333c3ddc26bef9d6081ddde21084b9f098f4f2
rlmgtr <rlmagtira@gmail.com>
2024-03-26 00:59:25 +0800

    hw

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,1 +1,21 @@
-// 1:54
+
+let initialValue = 0;
+
+let value = document.getElementById("value");
+
+value.innerText = initialValue;
+
+let incBtn = document.querySelector('[data-action="increment"]');
+let decBtn = document.querySelector('[data-action="decrement"]');
+
+incBtn.onclick = function() {
+initialValue += 1;
+value.innerText = initialValue
+};
+
+decBtn.onclick = function() {
+    if (initialValue > 0) {
+    initialValue -= 1;
+    value.innerText = initialValue 
+}
+};
